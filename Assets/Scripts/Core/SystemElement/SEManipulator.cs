f10a7cad590857b0086d3332ec3db158512ed320
Whigle <i.bro.n.man@gmail.com>
2026-04-17 13:40:09 +0200

    Removed unused fields from SEManipulator

--- Assets/Scripts/Core/SystemElement/SEManipulator.cs
+++ Assets/Scripts/Core/SystemElement/SEManipulator.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 
 public class SEManipulator : MonoBehaviour
 {
-	public event Action OnMouseEntered;
-	public event Action OnMouseExited;
-
 	[SerializeField]
 	private Transform snapPoint;
 	[SerializeField]
 	private LayerMask layerMask;
 
-	private bool manipulate = false;
 	private Vector3 snapPointOffset;
 
 	private void Awake()
